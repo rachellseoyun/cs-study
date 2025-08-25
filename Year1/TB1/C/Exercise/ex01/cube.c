94dd38b321d3a1f08a1749c2ff542145eb0dd2e8
rachellseoyun <146392453+rachellseoyun@users.noreply.github.com>
2025-08-25 14:59:47 +0900

    Create cube.c
    solution

---
+++ Year1/TB1/C/Exercise/ex01/cube.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int cube(int n) {
+  return n * n * n;
+}
+
+int main() {
+  int n;
+  printf("Enter an integer:\n");
+  scanf("%d", &n);
+  printf("%d cubed is %d\n", n, cube(n));
+  return 0;
+}
