72ae3b4cb1947f2a8a3b43d11f1f1b9787e9eec1
rachellseoyun <rachellseoyun@users.noreply.github.com>
2025-08-26 22:45:23 +0900

    task remainder code

---
+++ Year1/TB1/C/Exercise/ex01/remainder.c
@@ -0,0 +1,19 @@
+//
+// Created by rache on 25. 8. 26..
+//
+
+#include <stdio.h>
+
+int remainder(int x, int y) {
+    return x % y;
+}
+
+int main() {
+    int x, y;
+    printf("Enter an integer:\n");
+    scanf("%d", &x);
+    printf("Enter a second integer:\n");
+    scanf("%d", &y);
+    prinf("The remainder of %d divided by %d is: %d\n", x, y, remainder(x,y));
+    return 0;
+}
